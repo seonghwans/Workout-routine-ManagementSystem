10bbb45ea02fa25188b54d4ab1e30c3cc8ee2f1a
seonghwans <park121559@gmail.com>
2022-03-09 21:25:48 +0900

    remove hello.java and add the Menuformanagenent.java

---
+++ src/MenuforManagement.java
@@ -0,0 +1,12 @@
+
+public class MenuforManagement {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add routine");
+		System.out.println("2. Delete routine");
+		System.out.println("3. Edit routine");
+		System.out.println("4. View routine");
+		System.out.println("5. exit");
+	}
+
+}
